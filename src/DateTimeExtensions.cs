287b749655f6c3a88f33a318a5493113d22b72bd
Nelson Nobre <technobre@gmail.com>
2022-05-15 14:51:49 +0100

    Added extension FromTimestampToDateTime

--- src/DateTimeExtensions.cs
+++ src/DateTimeExtensions.cs
@@ -5,11 +5,58 @@ namespace PowerUtils.Globalization
     public static class DateTimeExtensions
     {
         /// <summary>
-        /// Convert datetime to timestamp
+        /// Convert datetime to timestamp (Unix timestamp is seconds past epoch)
         /// </summary>
         /// <param name="dateTime">Datetime to convert</param>
         /// <returns>Timestamp</returns>
         public static ulong ToTimestamp(this DateTime dateTime)
             => Convert.ToUInt64(dateTime.Subtract(new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)).TotalMilliseconds);
+
+
+
+        /// <summary>
+        /// Convert timestamp (Unix timestamp is seconds past epoch) to datetime
+        /// </summary>
+        /// <param name="timestamp">Timestamp to convert</param>
+        /// <returns>DateTime</returns>
+        public static DateTime FromTimestampToDateTime(this int timestamp)
+            => FromTimestampToDateTime(Convert.ToDouble(timestamp));
+
+        /// <summary>
+        /// Convert timestamp (Unix timestamp is seconds past epoch) to datetime
+        /// </summary>
+        /// <param name="timestamp">Timestamp to convert</param>
+        /// <returns>DateTime</returns>
+        public static DateTime FromTimestampToDateTime(this uint timestamp)
+            => FromTimestampToDateTime(Convert.ToDouble(timestamp));
+
+        /// <summary>
+        /// Convert timestamp (Unix timestamp is seconds past epoch) to datetime
+        /// </summary>
+        /// <param name="timestamp">Timestamp to convert</param>
+        /// <returns>DateTime</returns>
+        public static DateTime FromTimestampToDateTime(this long timestamp)
+            => FromTimestampToDateTime(Convert.ToDouble(timestamp));
+
+        /// <summary>
+        /// Convert timestamp (Unix timestamp is seconds past epoch) to datetime
+        /// </summary>
+        /// <param name="timestamp">Timestamp to convert</param>
+        /// <returns>DateTime</returns>
+        public static DateTime FromTimestampToDateTime(this ulong timestamp)
+            => FromTimestampToDateTime(Convert.ToDouble(timestamp));
+
+        /// <summary>
+        /// Convert timestamp (Unix timestamp is seconds past epoch) to datetime
+        /// </summary>
+        /// <param name="timestamp">Timestamp to convert</param>
+        /// <returns>DateTime</returns>
+        public static DateTime FromTimestampToDateTime(this double timestamp)
+        {
+            var dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            dateTime = dateTime.AddSeconds(timestamp).ToLocalTime();
+
+            return dateTime;
+        }
     }
 }
--- tests/PowerUtils.Globalization.Tests/DateTimeExtensionsTests.cs
+++ tests/PowerUtils.Globalization.Tests/DateTimeExtensionsTests.cs
@@ -18,4 +18,72 @@ public class DateTimeExtensionsTests
         act.Should()
             .Be(1576138359000);
     }
+
+    [Fact]
+    public void ToTimestamp_Int_DateTime()
+    {
+        // Arrange
+        var timestamp = 1652622232;
+        
+
+
+        // Act
+        var act = timestamp.FromTimestampToDateTime();
+
+
+        // Assert
+        act.Should()
+            .Be(new DateTime(2022, 05, 15, 14, 43, 52));
+    }
+
+    [Fact]
+    public void ToTimestamp_UInt_DateTime()
+    {
+        // Arrange
+        var timestamp = 1652622232u;
+
+
+
+        // Act
+        var act = timestamp.FromTimestampToDateTime();
+
+
+        // Assert
+        act.Should()
+            .Be(new DateTime(2022, 05, 15, 14, 43, 52));
+    }
+
+    [Fact]
+    public void ToTimestamp_Long_DateTime()
+    {
+        // Arrange
+        var timestamp = 1652622232L;
+
+
+
+        // Act
+        var act = timestamp.FromTimestampToDateTime();
+
+
+        // Assert
+        act.Should()
+            .Be(new DateTime(2022, 05, 15, 14, 43, 52));
+    }
+
+    [Fact]
+    public void ToTimestamp_ULong_DateTime()
+    {
+        // Arrange
+        var timestamp = 1652622232uL;
+
+
+
+        // Act
+        var act = timestamp.FromTimestampToDateTime();
+
+
+        // Assert
+        act.Should()
+            .Be(new DateTime(2022, 05, 15, 14, 43, 52));
+    }
 }
